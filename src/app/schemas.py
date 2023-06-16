652a4a3d9c689206ac8351a56e97b01e1d7f8a76
jackwotherspoon <jackwoth@google.com>
2023-06-16 18:12:03 +0000

    feat: add schema for Expense class

---
+++ src/app/schemas.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from typing import Optional
+from uuid import UUID
+
+from pydantic import BaseModel
+
+class Expense(BaseModel):
+    id: UUID
+    cost: float
+    time_created: datetime
+    description: Optional[str] = None
+    category: str = "Misc"
