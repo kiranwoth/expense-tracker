1181ffff6bf7de45775d19c18ee9291623be598b
jackwotherspoon <jackwoth@google.com>
2023-07-04 18:53:00 +0000

    lint

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,6 +4,7 @@ from src.app.main import app
 
 client = TestClient(app)
 
+
 def test_hello_world():
     response = client.get("/")
     assert response.status_code == 200
@@ -14,14 +15,14 @@ def test_read_expenses():
     response = client.get("/expenses/")
     assert response.status_code == 200
     assert response.json() == {
-    "8509e457-f491-46b5-bad1-e8d0504db2bb": {
-        "cost": 80.0,
-        "description": "Gas fill up",
-        "category": "Gas",
-        "id": "8509e457-f491-46b5-bad1-e8d0504db2bb",
-        "time_created": "2023-06-18T15:56:46.218431+00:00",
+        "8509e457-f491-46b5-bad1-e8d0504db2bb": {
+            "cost": 80.0,
+            "description": "Gas fill up",
+            "category": "Gas",
+            "id": "8509e457-f491-46b5-bad1-e8d0504db2bb",
+            "time_created": "2023-06-18T15:56:46.218431+00:00",
+        }
     }
-}
 
 
 def test_read_expense():
@@ -43,11 +44,7 @@ def test_read_expense_item_not_found():
 
 
 def test_create_expense():
-    expense = {
-        "cost": 1400,
-        "description": "Rent money for July",
-        "category": "Rent"
-    }
+    expense = {"cost": 1400, "description": "Rent money for July", "category": "Rent"}
     response = client.post("/expenses", json=expense)
     assert response.status_code == 200
     new_expense = response.json()
@@ -58,7 +55,7 @@ def test_create_expense():
     assert new_expense == {
         "cost": 1400,
         "description": "Rent money for July",
-        "category": "Rent"
+        "category": "Rent",
     }
 
 
